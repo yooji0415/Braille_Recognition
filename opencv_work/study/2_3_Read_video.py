7adc09dd9935c8baf83beaa61ce3deb601cf75ae
yooji0415 <gch05406@gmail.com>
2022-03-19 23:59:10 +0900

    study OpenCV

---
+++ opencv_work/study/2_3_Read_video.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+cap = cv.VideoCapture("output.avi")
+
+if cap.isOpened() == False:
+    print("동영상을 찾을 수 없습니다")
+    exit(1)
+
+while True:
+    ret, img_frame = cap.read()
+    if ret == False:
+        print("동영상 파일 읽기 완료")
+        break
+
+    cv.imshow("Color", img_frame)
+    key = cv.waitKey(25)
+    if key == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
